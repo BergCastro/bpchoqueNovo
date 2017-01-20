8a956d69d37edc8e9a8027a543ae548d92acb473
BergCastro <lindembergncastro@outlook.com>
2017-01-20 16:45:50 -0300

    varias modificacoes

--- src/main/resources/db/migration/V09__inserir_grupos.sql
+++ src/main/resources/db/migration/V09__inserir_grupos.sql
@@ -1,2 +1,5 @@
 INSERT INTO grupo (codigo, nome) VALUES (1, 'Administrador');
 INSERT INTO grupo (codigo, nome) VALUES (2, 'Vendedor');
+INSERT INTO grupo (codigo, nome) VALUES (3, 'Estrategico');
+INSERT INTO grupo (codigo, nome) VALUES (4, 'Tatico');
+INSERT INTO grupo (codigo, nome) VALUES (5, 'Operacional');
